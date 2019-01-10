e74d805d32c5ee8a1760e7f7893810798e408c46
Jack Baron <jackmbaron@gmail.com>
2019-01-10 18:32:24 +0000

    OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquaters are working VEWY HAWD to fix this.

--- public/src/remote/remote.js
+++ public/src/remote/remote.js
@@ -11,7 +11,7 @@ const { USER_AGENT } = require('../constants.js')
 const safeDownload = async (url, binary = false) => {
   try {
     const resp = await fetch(url, { headers: { 'User-Agent': USER_AGENT } })
-    const body = binary ? await resp.blob() : await resp.json()
+    const body = binary ? await resp.buffer() : await resp.json()
 
     if (resp.status !== 200) throw new Error('Status not 200')
     else return { error: undefined, body }
