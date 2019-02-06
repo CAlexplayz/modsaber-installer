39c643a1c1001ed66eb26309421844fcf2c5d85c
Jack Baron <jackmbaron@gmail.com>
2019-02-06 14:04:17 +0000

    Convert Loading.jsx to a pure component

--- src/js/Loading.jsx
+++ src/js/Loading.jsx
@@ -1,30 +1,21 @@
-import React, { Component } from 'react'
-
-import Context from './Context.jsx'
+import React from 'react'
 import Icon from '../img/icon.png'
 
-class Loading extends Component {
-  static contextType = Context
-
-  render () {
-    return (
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-        height: '100%',
-        '-webkit-app-region': 'drag',
-      }}>
-        <img src={ Icon } style={{ width: '50%', height: 'auto' }} alt='ModSaber Logo' />
-        <div style={{ textAlign: 'center' }}>
-          <h1 className='is-size-4 has-text-weight-semibold'>ModSaber Installer</h1>
-          <h3><i className='fas fa-cog fa-spin'></i> Loading...</h3>
-        </div>
-      </div>
-    )
-  }
-}
+const Loading = () =>
+  <div style={{
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    '-webkit-app-region': 'drag',
+  }}>
+    <img src={ Icon } style={{ width: '50%', height: 'auto' }} alt='ModSaber Logo' />
+    <div style={{ textAlign: 'center' }}>
+      <h1 className='is-size-4 has-text-weight-semibold'>ModSaber Installer</h1>
+      <h3><i className='fas fa-cog fa-spin'></i> Loading...</h3>
+    </div>
+  </div>
 
 export default Loading
