ef549dffe94c19a6de3ad1e0517fac804ff0c603
Jack Baron <jackmbaron@gmail.com>
2019-02-07 03:06:48 +0000

    Convert index to TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './js/App'
 import Events from './js/Events'
 import Splash from './js/Splash'
-import { Provider } from 'react-redux'
 import { store } from './js/store'
 
-import '@lolpants/bulma/css/bulma.css'
 import '@fortawesome/fontawesome-free/css/all.css'
-import './css/styles.css'
+import '@lolpants/bulma/css/bulma.css'
 import './css/dark.css'
+import './css/styles.css'
 import './css/tools.css'
 
 import './js/shortcuts.js'
@@ -17,12 +17,15 @@ import './js/shortcuts.js'
 import { getCurrentWindow } from './js/utils/electron'
 const loading = getCurrentWindow().custom.ROLE === 'WINDOW_LOADING'
 
-const Root = () => loading ?
-  <Splash /> :
-  <Provider store={ store }>
-    <Events>
-      <App />
-    </Events>
-  </Provider>
+const Root = () =>
+  loading ? (
+    <Splash />
+  ) : (
+    <Provider store={store}>
+      <Events>
+        <App />
+      </Events>
+    </Provider>
+  )
 
-ReactDOM.render(<Root/>, document.getElementById('root'))
+ReactDOM.render(<Root />, document.getElementById('root'))
